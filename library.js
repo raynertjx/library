0d214129a8e3131c806e88b8c0c09606e0f0a871
raynertjx <rayner742@gmail.com>
2021-12-19 15:24:23 +0800

    Modified delete book function, basic functionality restored

--- library.js
+++ library.js
@@ -5,21 +5,54 @@ const bookAuthor = document.getElementById("bookauthor");
 const bookPages = document.getElementById("bookpages");
 const checkBox = document.getElementById("checkbox");
 const table = document.getElementById("table");
-const tableBody = document.getElementById("tablebody");
+const tableBody = document.getElementById("book-table-body");
 
 function Book(title, author, pages, readstatus) {
   this.title = title
   this.author = author
   this.pages = pages
   this.readstatus = readstatus
-}
+  }
 
 let myLibrary = [];
 
 myLibrary[0] = new Book ("1984", "George Orwell", "326", "Read");
 myLibrary[1] = new Book ("Fahrenheit 451", "Ray Bradbury", "256", "Read");
 
-displayBooks();
+table.addEventListener("click", (e) => {
+  const currentTarget = e.target.parentNode.parentNode.childNodes[1]
+  if (e.target.classList.contains("readbookbutton")) {
+    changeStatus(findBook(myLibrary, currentTarget.innerText));
+  }
+  if (e.target.classList.contains("deletebookbutton")) {
+    deleteBook(findBook(myLibrary, currentTarget.innerText));
+  }
+  resetTable();
+  displayBooks()
+})
+
+function changeStatus(book) {
+  if (myLibrary[book].readstatus === "Read") {
+    myLibrary[book].readstatus = "Not Read";
+  } else {
+    myLibrary[book].readstatus = "Read";
+  }
+}
+
+function deleteBook(book) {
+  myLibrary.splice(book, book + 1);
+}
+
+function findBook(libraryArray, name) {
+  if (libraryArray.length === 0 || libraryArray === null) {
+    return;
+  }
+  for (var book of libraryArray) {
+    if (book.title === name) {
+      return libraryArray.indexOf(book)
+    }
+  }
+}
 
 function addBookToLibrary() {
   if (checkBox.checked == true) {
@@ -31,42 +64,35 @@ function addBookToLibrary() {
   myLibrary.push(new Book(bookTitle.value, bookAuthor.value, bookPages.value, checkBox.value));
 }
 
-function deleteBook(o) {
-  var p = o.parentNode.parentNode;
-  p.parentNode.removeChild(p);
-}
 
 function resetTable() {
-  $("#table tbody tr").remove();
+  $("#book-table-body tr").remove();
 }
 
 function displayBooks() {
   myLibrary.forEach(function (book) {
-    var row = tableBody.insertRow();
-  
-    indexOfRow = row.rowIndex;
-
-    var cell1 = row.insertCell(0);
-    var cell2 = row.insertCell(1);
-    var cell3 = row.insertCell(2);
-    var cell4 = row.insertCell(3);
-    var cell5 = row.insertCell(4);
     
-    cell1.innerHTML = book.title
-    cell2.innerHTML = book.author
-    cell3.innerHTML = book.pages
-    cell4.innerHTML = '<button id="readbookbutton"></button>'
-    var readBookBtn = document.querySelectorAll("#readbookbutton");
-    readBookBtn.forEach(button => {
-      button.innerHTML = book.readstatus
-    })
-    cell5.innerHTML = '<button id="deletebookbutton" onclick="deleteBook(this)">D</button>'
+    const htmlBook = `
+    <tr>
+    <td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${book.pages}</td>
+    <td><button class="readbookbutton">${book.readstatus}</button></td>
+    <td><button class="deletebookbutton">D</button></td>
+    </tr>
+    `;
+    tableBody.insertAdjacentHTML("afterbegin", htmlBook);
   })
 }
-
-
+displayBooks();
 
 submitBtn.addEventListener("click", () => {
+  for (var book of myLibrary) {
+    if (book.title === bookTitle.value) {
+      alert("Book already exists in library!");
+      return;
+    }
+  }
   resetTable();
   addBookToLibrary();
   displayBooks();
